1e1a2d53d3b1905e4e871ffab33ca31f5ac3677b
Felipe Pardo <fpardo@UTESALTA.NET>
2022-09-28 16:49:53 -0300

    routes cart fixed

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -5,10 +5,10 @@ const middlewares = require("../middlewares/middlewares");
 
 //? CART
 
-router.post("/");
-router.delete("/:id");
-router.get("/:id/products");
-router.post("/:id/products");
-router.delete("/:id/products/:id_prod");
+router.post("/", controller.newCart); // TODO Agregar carro finalizado
+router.delete("/:id", controller.delete); // TODO ELiminar carro - finalizado
+// router.get("/:id/products", controller.productsInCart); //TODO Ver los productos guardados en el carrito
+router.post("/:id/products"); // TODO Agregar un producto al carro por su ID
+router.delete("/:id/products/:id_prod"); // TODO Eliminar un producto de un carrito por su id de producto y de carrito
 
 module.exports = router;
