1976591ec06d69c1424b486c8624b8356478dc8b
Felipe Pardo <fpardo@UTESALTA.NET>
2022-11-03 15:57:13 -0300

    daos Mongo

--- src/daos/index.js
+++ src/daos/index.js
@@ -1,42 +1,25 @@
-// //! __DIRNAME PATH
-// import path from "path";
-// import { fileURLToPath } from "url";
-// const filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(filename);
-// //! DOTENV
+//! DOTENV
 import * as dotenv from "dotenv";
 process.env.NODE_ENV
-  ? dotenv.config({path:`.env.${process.env.NODE_ENV}`})
+  ? dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
   : dotenv.config();
-  console.log(`.env.${process.env.NODE_ENV}`, "ruta en index")
+console.log(`.env.${process.env.NODE_ENV}`, "ruta en index");
 
-let DAOProducts, DAOCarts
-
-console.log(process.env.TYPE, "type wrong") // TODO wrong
+let DAOProducts, DAOCarts;
 
 switch (process.env.TYPE) {
-    case 'file':
-        const { default: DAOProductsFile } = await import('./file.DAO.products.js')
-        const { default: DAOCartsFile } = await import('./file.DAO.cart.js')
+  case "firebase":
+    break;
+  case "mongodb":
+    break;
 
-        DAOProducts = new DAOProductsFile()
-        DAOCarts = new DAOCartsFile()
-        break
-    case 'firebase':
-        
-        break
-    case 'mongodb':
-        
-        break
-    case 'mariadb':
-        
-        break
-    case 'sqlite3':
-        
-        break
-    default:
-        
-        break
+  default:
+    const { default: DAOProductsFile } = await import("./file.DAO.products.js");
+    const { default: DAOCartsFile } = await import("./file.DAO.cart.js");
+
+    DAOProducts = new DAOProductsFile();
+    DAOCarts = new DAOCartsFile();
+    break;
 }
 
-export { DAOProducts, DAOCarts }
+export { DAOProducts, DAOCarts };
