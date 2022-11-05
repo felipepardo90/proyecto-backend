ca0f7da0eefdb1fa3631fdc40c3be41b6f8d4b9e
Felipe Pardo <felipepardo90@hotmail.com>
2022-11-05 19:34:36 -0300

    mongo Persistances in Cart

--- src/models/Mongo Pers/Cart.js
+++ src/models/Mongo Pers/Cart.js
@@ -7,13 +7,14 @@ export default class Cart {
   constructor(coll, schema) {
     this.db = mongoose.model(coll, schema);
     this.date = new Date().toLocaleString();
-    this.products = [];
     this.total = 0;
+    this.products = Array;
   }
 
   async newCart() {
     try {
-      const newCart = await this.db.create({// TODO OPTIMIZAR
+      const newCart = await this.db.create({
+        // TODO OPTIMIZAR
         timestamp: this.date,
         products: this.products,
         total: this.total,
@@ -24,7 +25,8 @@ export default class Cart {
     }
   }
 
-  async deleteCartById(idEntered) {// TODO OPTIMIZAR
+  async deleteCartById(idEntered) {
+    // TODO OPTIMIZAR
     try {
       await this.db.deleteOne({ _id: idEntered });
       return idEntered;
@@ -33,7 +35,8 @@ export default class Cart {
     }
   }
 
-  async getCartById(idEntered) {// TODO OPTIMIZAR
+  async getCartById(idEntered) {
+    // TODO OPTIMIZAR
     try {
       const data = await this.db.find({ _id: idEntered });
       return data[0];
@@ -42,12 +45,18 @@ export default class Cart {
     }
   }
 
-  async addProductToCart(idEntered, object) {// TODO OPTIMIZAR
+  async addProductToCart(idEntered, object) {
+    // TODO OPTIMIZAR
     try {
       await this.db.updateOne(
         { _id: idEntered },
-        { $set: { products: object } }
+        {
+          $push: {
+            products: object[0],
+          },
+        }
       );
+
       return await this.db.findOne({ _id: idEntered });
     } catch (error) {
       console.error(`Se produjo un error en addProductToCart:${error}`);
@@ -56,15 +65,18 @@ export default class Cart {
 
   async deleteProductInCartById(idCart, idProduct) {
     try {
+      await this.db.updateOne(
+        { _id: idCart },
+        {
+          $pull: {
+            products: { _id: { idProduct } },
+          },
+        }
+      );
 
-      const data = await this.db.find({ _id: idEntered }); // TODO ARREGLAR QUE ESTÁ MALÏSIMO
-
-      await this.db.deleteOne({ _id: idCart }, {_id:idEntered});
-
-      return data[0];
-
+      return { title: "Classic Guitar" };
     } catch (error) {
-      console.error(`Se produjo un error en deleteProductInCartById:${error}`);
+      console.error(`Se produjo un error en deleteProductInCartById: ${error}`);
     }
   }
 }
