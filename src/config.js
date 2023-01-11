e63ef02b79db4802aeea4626c20087c3b8648d40
Felipe Pardo <fpardo@UTESALTA.NET>
2023-01-11 16:40:47 -0300

    config db

--- src/config.js
+++ src/config.js
@@ -1,27 +1,10 @@
 const Config = {
-  firebase: {},
   mongodb: {
     url: "mongodb://localhost:27017/ecommerce",
     options: {
       serverSelectionTimeoutMS: 5000,
     },
-  },
-  mariadb: {
-    client: "mysql",
-    connection: {
-      host: "localhost",
-      port: 3306,
-      user: "root",
-      database: "coderhouse-db",
-    },
-  },
-  sqlite: {
-    client: "sqlite3",
-    connection: {
-      filename: "./src/DB/ecommerce.sqlite3",
-    },
-    useNullAsDefault: true,
-  },
+  }
 };
 
 export default Config
