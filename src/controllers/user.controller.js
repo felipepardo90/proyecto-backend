c5c2d109931952b9574601d5a30451720091caea
Felipe Pardo <fpardo@UTESALTA.NET>
2023-01-27 16:13:30 -0300

    controllers for users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,5 @@
+import passport from "passport";
+
 const controller = {};
 
 //! REGISTER
@@ -6,11 +8,23 @@ controller.renderRegistryView = (req, res) => {
   res.render("register");
 };
 
+controller.signUpUser = passport.authenticate("signup", {
+  successRedirect: "/login",
+  failureRedirect: "/register",
+  passReqToCallback: true,
+});
+
 //! LOGIN
 
 controller.renderLoginView = (req, res) => {
   res.render("login");
 };
+
+controller.logInUser = passport.authenticate("login", {
+  successRedirect: "/profile",
+  failureRedirect: "/login",
+  passReqToCallback: true,
+});
 
 //! PROFILE
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,39 +1,17 @@
 import { Router } from "express";
-import passport from "passport";
 import controller from "../controllers/user.controller.js";
 import { MW } from "../libs/middlewares.js";
 const router = Router();
 
 //! Register
-
 router.get("/register", MW.isNotAuth, controller.renderRegistryView);
-router.post(
-  "/register",
-  passport.authenticate("signup", {
-    successRedirect: "/login",
-    failureRedirect: "/register",
-    passReqToCallback: true,
-  })
-);
-
+router.post("/register", controller.signUpUser);
 //! Login
-
 router.get("/login", controller.renderLoginView);
-router.post(
-  "/login",
-  passport.authenticate("login", {
-    successRedirect: "/profile",
-    failureRedirect: "/login",
-    passReqToCallback: true,
-  })
-);
-
+router.post("/login", controller.logInUser);
 //! Logout
-
 router.get("/logout", controller.logoutUser);
-
 //! Profile
-
 router.get("/profile", MW.isAuth, controller.renderProfileView);
 
 export default router;
