3e2448c48d09be118320207705ce3d15175c7dc6
Felipe Pardo <fpardo@UTESALTA.NET>
2023-03-03 15:52:51 -0300

    new keys

---
+++ src/libs/keys.js
@@ -0,0 +1,25 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export let PORT;
+export let MONGODB_URI
+export let MONGO_SESSION
+export let ENV
+export let SECRET = process.env.SECRET;
+switch (process.env.NODE_ENV) {
+  case "prod":
+    PORT = process.env.PORT;
+    MONGODB_URI = process.env.MONGODB_URI
+    MONGO_SESSION = process.env.MONGO_SESSION
+    ENV = process.env.NODE_ENV
+    break;
+
+  default:
+    PORT = 8000;
+    MONGODB_URI = "mongodb://localhost:27017/ecommerce"
+    MONGO_SESSION = "mongodb://localhost:27017/sessions"
+    ENV = "dev"
+    break;
+}
+
+
