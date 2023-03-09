ed778d64a19412fff4e5d7712a4a888a2d69e55c
Felipe Pardo <felipepardo90@hotmail.com>
2023-03-08 23:53:32 -0300

    chat model

---
+++ src/models/Mongo Pers/Chat.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import config from "../../libs/config.js";
+
+await mongoose.connect(config.mongodb.url, config.mongodb.options);
+
+export default class Chat {
+    constructor(coll, schema) {
+        this.db = mongoose.model(coll, schema);
+    }
+
+    async saveMessage(messageInfo) {
+        try {
+            await this.db.create({ ...messageInfo, timestamp: this.date })
+            return await this.db.find({})
+        } catch (error) {
+            console.error(`Se produjo un error en saveMessage:${error}`);
+        }
+    }
+
+    async readMessages() {
+        try {
+            return await this.db.find({})
+        } catch (error) {
+            console.error(`Se produjo un error en readMessages:${error}`);
+        }
+    }
+}
