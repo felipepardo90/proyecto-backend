886bddfefd4bb80215adb025c96efa3e332df9aa
Felipe Pardo <felipepardo90@hotmail.com>
2023-03-08 23:52:51 -0300

    info server

---
+++ src/controllers/info.controller.js
@@ -0,0 +1,22 @@
+import { cpus } from "os";
+const cpu = cpus();
+
+
+const renderServerInfo = (req, res) => {
+    res.send({
+        info: {
+            "Input Arguments": process.argv.slice(2),
+            OS: process.platform,
+            "Node Version": process.version,
+            "Memory Usage": process.memoryUsage().rss,
+            "Execution Path": process.execPath,
+            "Process ID": process.pid,
+            "Current Working Directory": process.cwd(),
+            "active processors": cpu.length,
+        },
+    });
+};
+
+export default renderServerInfo
+
+
