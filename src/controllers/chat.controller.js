edd6c07f9016a1972ee6a3d83514019e76d3d6da
Felipe Pardo <felipepardo90@hotmail.com>
2023-03-08 23:55:58 -0300

    chat controller

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,5 +1,25 @@
-const renderChat = (req, res) => {
-    res.render("chat")
-};
+//! WEBSOCKETS
+import io from "../server.js";
 
-export default renderChat
+let controller = {}
+
+io.emit("connection", socket => {
+    controller.renderChat = async (req, res) => {
+        socket.on("chat:history", messages => {
+            res.render("chat", { messages: messages })
+        })
+    };
+
+    controller.sendMessage = async (req, res) => {
+        const { message } = req.body
+        const username = req.user.email
+
+        socket.emit("chat:message", {
+            username,
+            message,
+            date: new Date().toLocaleString()
+        });
+    };
+})
+
+export default controller
