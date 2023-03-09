0a1dd38c2fdba5e2177af5e3f1c3b7b79d36b90a
Felipe Pardo <felipepardo90@hotmail.com>
2023-03-08 23:55:26 -0300

    server sockets

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 import app from "./app.js";
 import { ENV } from "./libs/keys.js";
+import { DAOChats } from "./daos/index.js";
+
+//! WEBSOCKETS
+import { Server as WebSocketServer } from "socket.io";
+import http from "http";
+const server = http.createServer(app);
+const io = new WebSocketServer(server);
 
 //! STARTING SERVER
 
-const server = app.listen(app.get("port"), () => {
+server.listen(app.get("port"), () => {
   console.log(`Express server started at ${ENV} environment on port ${app.get("port")}`);
 });
 
@@ -12,3 +19,29 @@ const server = app.listen(app.get("port"), () => {
 server.on("error", (error) => {
   console.log(`Error !!!: ${error}`);
 });
+
+io.on("connection", async (socket) => {
+  //! Nueva conexión
+  console.log(`New Connection: ${socket.id}`);
+
+  //! CHAT
+
+  //! El evento chat:messages iniciará enviando el array existente al cliente
+  const allMessages = await DAOChats.readMessages();
+  socket.emit("chat:history", allMessages);
+
+  //! Se escucha el evento chat:message, se guarda el mensaje recibido por el cliente y se emite un mensaje general con el array Messages actualizado a todos los sockets conectados y por conectarse
+
+  socket.on("chat:message", async (data) => {
+    const allMessages = await DAOChats.saveMessage(data);
+    io.sockets.emit("chat:history", allMessages);
+  });
+
+  //! Se escucha el evento chat:typing y se emite un mensaje a todos los sockets conectados, excepto al que "está escribiendo..." con el método broadcast
+
+  socket.on("chat:typing", (data) => {
+    socket.broadcast.emit("chat:typing", data);
+  });
+});
+
+export default io
