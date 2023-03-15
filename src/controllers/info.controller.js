d55fae0b38cb70dc22a922d630f849512749c379
Felipe Pardo <felipepardo90@hotmail.com>
2023-03-14 23:38:12 -0300

    info controller & views

--- src/controllers/info.controller.js
+++ src/controllers/info.controller.js
@@ -1,18 +1,19 @@
 import { cpus } from "os";
 const cpu = cpus();
 
+const info = {
+  "Input Arguments": process.argv.slice(2),
+  OS: process.platform,
+  "Node Version": process.version,
+  "Memory Usage": process.memoryUsage().rss,
+  "Execution Path": process.execPath,
+  "Process ID": process.pid,
+  "Current Working Directory": process.cwd(),
+  "active processors": cpu.length,
+}
+
 const renderServerInfo = (req, res) => {
-  res.status(200).render("info", {
-      "Input Arguments": process.argv.slice(2),
-      OS: process.platform,
-      "Node Version": process.version,
-      "Memory Usage": process.memoryUsage().rss,
-      "Execution Path": process.execPath,
-      "Process ID": process.pid,
-      "Current Working Directory": process.cwd(),
-      "active processors": cpu.length,
-    
-  });
+  res.status(200).render("info", { info: info });
 };
 
 export default renderServerInfo;
