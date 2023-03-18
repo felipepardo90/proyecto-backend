29fb78fa57a91646e83a3aa2663f0b5117c7e1c4
Felipe Pardo <felipepardo90@hotmail.com>
2023-03-18 16:34:50 -0300

    changing  Cart model

--- src/models/Mongo Pers/Cart.js
+++ src/models/Mongo Pers/Cart.js
@@ -9,9 +9,9 @@ export default class Cart {
     this.date = new Date().toLocaleString();
   }
 
-  async newCart(id) {
+  async newCart() {
     try {
-      return await this.db.create({ owner_id: id, timestamp: this.date })
+      return await this.db.create({ timestamp: this.date })
     } catch (error) {
       console.error(`Se produjo un error en save:${error}`);
     }
