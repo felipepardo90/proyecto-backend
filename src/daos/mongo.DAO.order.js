a81fa54a8f0f1960889ea2f60a23efc0b414dcc7
Felipe Pardo <felipepardo90@hotmail.com>
2023-03-22 06:48:46 -0300

    dao order mongo

--- src/daos/mongo.DAO.order.js
+++ src/daos/mongo.DAO.order.js
@@ -4,8 +4,8 @@ class DAOOrdersMongo extends Order {
   constructor() {
     super("orders", {
       owner: { type: String, required: true },
-      email: { type: String, required: true },
-      address: { type: String, required: true },
+      email: { type: String },
+      address: { type: String },
       date: { type: String },
       products: { type: Array, required: true },
       shipment: { type: Number, required: true },
